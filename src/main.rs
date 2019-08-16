50dfa6e7935a82079ba9ed7a5ce4913e42f024d4
Ryan Schmukler <rschmukler@gmail.com>
2019-08-15 20:57:01 -0400

    feat: add pid monitoring
    Adds a `--pid` flag to monitor for process termination

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,36 @@ mod client;
 use client::{Command, XIdleHookClient};
 use crossbeam_channel::{after, bounded, never, select, Receiver};
 use std::time::Duration;
+use std::path::Path;
+use std::thread;
 
 fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
-    let (sender, receiver) = bounded(100);
+    let (sender, receiver) = bounded(2);
     ctrlc::set_handler(move || {
         let _ = sender.send(());
     })?;
 
     Ok(receiver)
+}
+
+fn pid_channel(pid: u64) -> Option<Receiver<()>> {
+    let (sender, receiver) = bounded(1);
+    let path_str: String = format!("/proc/{:?}", pid);
+    let path = Path::new(&path_str);
+    if !path.exists() {
+        return None
+    }
+    thread::spawn(move ||{
+        let path = Path::new(&path_str);
+        loop {
+            if !path.exists() {
+                sender.send(()).unwrap();
+                break;
+            }
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+    Some(receiver)
 }
 
 fn main() {
@@ -37,12 +59,20 @@ fn main() {
                 .help("Wait a specified number of seconds")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("pid")
+                .short("p")
+                .long("pid")
+                .value_name("PROCESS_ID")
+                .help("Wait for a process to quit")
+                .takes_value(true),
+        )
         .get_matches();
 
     let socket = matches.value_of("socket").expect("no socket path provided");
     let mut client = XIdleHookClient::new(socket).expect("Error connecting to xidlehook socket");
 
-    let quit_timer = if matches.is_present("timer") {
+    let timer_event = if matches.is_present("timer") {
         let secs = value_t_or_exit!(matches, "timer", u64);
         let duration = Duration::new(secs, 0);
         after(duration)
@@ -55,9 +85,20 @@ fn main() {
         .send(Command::Disable)
         .expect("error communicating with xidlehook");
 
+    let (pid_event, pid) = if matches.is_present("pid") {
+        let pid = value_t_or_exit!(matches, "pid", u64);
+        let pid_event = pid_channel(pid).expect(&format!("Process with pid {:?} does not exist", pid));
+        (pid_event, Some(pid))
+    } else {
+        (never(), None)
+    };
+
     select! {
-        recv(quit_timer) -> _ => {
+        recv(timer_event) -> _ => {
             println!("Times up! Goodbye");
+        }
+        recv(pid_event) -> _ => {
+            println!("Process {:?} exited...", pid);
         }
         recv(ctrl_c_event) -> _ => {
             println!("Shutting down...");
